e2fd98976d20c89a9c79ababb3bdec326408915c
RastogiTanya <rastogitanya96@gmail.com>
2021-06-19 00:22:17 +0530

    Header footer updated

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Navbar, Nav, Container } from "react-bootstrap";
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="light" expand="lg" variant="light" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Webiste Spot</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+              <Nav.Link href="/signin">
+                <i className="fas fa-user"></i>Sign-In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
