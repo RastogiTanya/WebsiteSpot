a800eb53d1ee767aba9ab19b3442cf899c9ff866
RastogiTanya <rastogitanya96@gmail.com>
2021-06-20 23:13:42 +0530

    backend fronend in sync

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+
 app.get("/", (req, res) => {
   res.send("Api is running");
 });
@@ -12,4 +15,9 @@ app.get("/api/products/:id", (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
-app.listen(5000, console.log("server running on port 5000"));
+
+const port = process.env.PORT || 5000;
+app.listen(
+  port,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+);
