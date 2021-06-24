a80d89f4a880d32c2d39fa39a40e67d751d82d5d
RastogiTanya <rastogitanya96@gmail.com>
2021-06-24 20:18:52 +0530

    components updated

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -7,21 +7,29 @@ import { listProducts } from "../actions/productActions.js";
 const Homescreen = () => {
   const dispatch = useDispatch();
 
+  const productList = useSelector((state) => state.productList);
+  const { loading, error, products } = productList;
   useEffect(() => {
     dispatch(listProducts());
   }, [dispatch]);
 
-  const products = [];
+  // const products = [];
   return (
     <>
       <h1>Latest websites</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   );
 };
