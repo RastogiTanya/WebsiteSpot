f21ad8694fb00d73604b813743dd326d7b7ad237
RastogiTanya <rastogitanya96@gmail.com>
2021-07-10 15:40:48 +0530

    paid and delivery information added

--- frontend/src/screens/OrderScreen.js
+++ frontend/src/screens/OrderScreen.js
@@ -8,11 +8,23 @@ import CheckoutSteps from "../components/CheckoutSteps";
 import { getOrderDetails } from "../actions/orderActions";
 const OrderScreen = ({ match }) => {
 	const orderId = match.params.id;
-    
+
 	const dispatch = useDispatch();
 
 	const orderDetails = useSelector((state) => state.orderDetails); //store se data laaya
 	const { order, loading, error } = orderDetails;
+
+	if (!loading) {
+		const addDecimals = (num) => {
+			return (Math.round(num * 100) / 100).toFixed(2);
+		};
+		order.itemsPrice = addDecimals(
+			order.orderItems.reduce(
+				(acc, item) => acc + item.price * item.qty,
+				0
+			)
+		);
+	}
 
 	useEffect(() => {
 		console.log("object");
@@ -32,17 +44,46 @@ const OrderScreen = ({ match }) => {
 						<ListGroup.Item>
 							<h2>Shipping</h2>
 							<p>
-								<strong>Address</strong>
+								<strong>Name: </strong>
+								{order.user.name}
+							</p>
+							<p>
+								{" "}
+								<strong>Email: </strong>
+								<a href={`mailto:${order.user.email}`}>
+									{order.user.email}
+								</a>
+							</p>
+							<p>
+								<strong>Address: </strong>
 								{order.shippingAddress.address},
 								{order.shippingAddress.city},
 								{order.shippingAddress.postalCode},
 								{order.shippingAddress.country}
 							</p>
+							{order.isDelivered ? (
+								<Message variant="success">
+									Delivered on {order.deliveredAt}
+								</Message>
+							) : (
+								<Message variant="danger">
+									Not Delivered
+								</Message>
+							)}
 						</ListGroup.Item>
 						<ListGroup.Item>
 							<h2>Payment Method</h2>
-							<strong>Method: </strong>
-							{order.paymentMethod}
+							<p>
+								<strong>Method: </strong>
+								{order.paymentMethod}
+							</p>
+							{order.isPaid ? (
+								<Message variant="success">
+									Paid on {order.paidAt}
+								</Message>
+							) : (
+								<Message variant="danger">Not paid</Message>
+							)}
 						</ListGroup.Item>
 						<ListGroup.Item>
 							<h2>Your Items</h2>
