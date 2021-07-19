a04e9017feb1d9ba2625184877c0ab36b9e27962
RastogiTanya <rastogitanya96@gmail.com>
2021-07-19 19:04:36 +0530

    Product list screen done

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
+import path from "path";
 import express from "express";
 import connectDB from "./config/DB.js";
 import colors from "colors";
+import morgan from "morgan";
 import dotenv from "dotenv";
+
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
@@ -11,10 +14,11 @@ dotenv.config();
 
 connectDB();
 const app = express();
+
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
 app.use(express.json());
-app.get("/", (req, res) => {
-	res.send("Api is running");
-});
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
@@ -22,8 +26,21 @@ app.use("/api/orders", orderRoutes);
 
 app.get("/api/config/paypal", (req, res) =>
 	res.send(process.env.PAYPAL_CLIENT_ID)
-); 
+);
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+	app.get("*", (req, res) =>
+		res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+	);
+} else {
+	app.get("/", (req, res) => {
+		res.send("API is running....");
+	});
+}
 app.use(notFound);
 
 app.use(errorHandler);
