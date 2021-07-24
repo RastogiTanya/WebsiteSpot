4fe6b0470149b87b3a83f7830e000879a55b6b38
RastogiTanya <rastogitanya96@gmail.com>
2021-07-24 19:15:32 +0530

    reviews added from frontend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,11 +6,13 @@ import {
 	deleteProduct,
 	updateProduct,
 	createProduct,
+	createProductReview,
 } from "../controllers/productController.js";
 import { protect, admin } from "../middleware/authMiddleware.js";
 //description  : fetch all products
 
 router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
 
 router
 	.route("/:id")
